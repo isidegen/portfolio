62f0e957476791de27c45dd17ed173a27584888f
isidegen <99802062+isidegen@users.noreply.github.com>
2024-05-27 16:48:37 +0200

    first commit

---
+++ portfolio/nuxt.config.js
@@ -0,0 +1,15 @@
+export default defineNuxtConfig({
+  css: ['@/assets/css/roboto.css'],
+  modules: [
+    [
+      '@storyblok/nuxt',
+      {
+        accessToken: 'S9vbYbLDEBzJ6b7Dx1Adnwtt',
+        apiOptions: {
+          region: '' // Set 'US" if your space is created in US region (EU default)
+        }
+      },
+    ],
+    '@nuxtjs/tailwindcss',
+  ]
+})
